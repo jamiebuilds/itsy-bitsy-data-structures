9f48f98e40207fc3cd1612509b47ee9864d3889b
Brian Fryer <brianfryer@gmail.com>
2016-08-27 01:47:13 -0500

    Fix typos (#3)
    * Fix for typo
    
    "out" should be "our"
    
    * Fix for typo
    
    "like" should be "life"

--- itsy-bitsy-data-structures.js
+++ itsy-bitsy-data-structures.js
@@ -574,7 +574,7 @@ class HashTable {
   }
 
   /**
-   * Next, lets define out "get" function so we have a way of accessing values
+   * Next, lets define our "get" function so we have a way of accessing values
    * by their key.
    *
    * HashTable access is constant O(1) - "AWESOME!!"
@@ -1107,7 +1107,7 @@ class LinkedList {
  * The remaining two data structures we are going to cover are both in the
  * "tree" family.
  *
- * Much like real like, there are many different types of tree data structures.
+ * Much like real life, there are many different types of tree data structures.
  *
  *     Binary Trees:
  *       AA Tree, AVL Tree, Binary Search Tree, Binary Tree, Cartesian Tree,
